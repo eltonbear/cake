48c6eaf11a83fd88b31d1fc2734b5e704486ce4b
SHON <elton.shon@keysight.com>
2016-10-12 00:59:21 -0700

    done

---
+++ excel.py
@@ -0,0 +1,63 @@
+import pyexcel as pe
+import warnings
+
+def readSheet(filePath):
+	titleRow = 1
+	try:
+		sheet = pe.get_sheet(file_name=filePath, name_columns_by_row = titleRow)
+	except NotImplementedError as error:
+		return {'error': 'No source found for ' + filePath}
+	except FileNotFoundError as error:
+		return {'error': error.args[1] + ': ' + filePath}
+
+	titleRow = -1
+	productInfoC = 0
+	productInfoR = 0
+	emptyRow = 1
+	titles = ['Ref Des', 'Part Number', 'X-location', 'Y-location', 'Rotation', 'x']
+	
+	emptyRowList = sheet.row[emptyRow]
+	if not all(isinstance(e, str) for e in emptyRowList):
+		return{'error': 'File format incorrect: ' + filePath}
+	spaceString = ""
+	for empty in emptyRowList:
+		spaceString = spaceString + empty
+
+	if type(spaceString) is not str or spaceString.strip() != '':
+		return{'error': 'File format incorrect: ' + filePath}
+
+	try:
+		productionInfo = sheet.row[productInfoR][productInfoC].split( )
+		productName = productionInfo
+		del sheet.row[emptyRow]
+		del sheet.row[productInfoR]
+		print(sheet.row[0])
+
+		dataDictionary = sheet.to_dict()
+
+		keys = list(dataDictionary.keys())
+		for key in keys:
+			if key[0] == ' ' or key[-1] == ' ':
+				dataDictionary[key.strip()] = dataDictionary.pop(key)
+	except:
+		return {'error': 'File format incorrect: ' + filePath}
+
+	missing = []
+	for title in titles:
+		if title not in dataDictionary:
+			missing.append(title)
+
+	if missing:
+		return dataDictionary
+	else:
+		return {'error': missing}
+
+# a = readSheet(r"C:\/Users\eltoshon\Desktop\pico_top_expanded.csv")
+a = readSheet("C:/Users/eltoshon/Desktop/pico_top_expanded.xlsx")
+# b = readSheet("C:/Users/eltoshon/Desktop/TTTTTTTest_instruction.xlsm")
+# print(a['error'])
+
+
+
+
+
