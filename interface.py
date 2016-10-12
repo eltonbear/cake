6d2fe1c59a6b0f088b60e8cbff0cd6b1d007cc8e
SHON <elton.shon@keysight.com>
2016-10-12 16:01:26 -0700

    roughly finished basic funcs of 3 interfaces, need to find a way to show them in order

--- interface.py
+++ interface.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter.filedialog import askopenfilename, askdirectory, asksaveasfilename
+from tkinter import messagebox
+from os.path import exists
 
 class browse(Frame):
 
@@ -49,7 +52,7 @@ class browse(Frame):
 	# 	self.filePathEntry.delete(0, 'end')
 	# 	self.filePathEntry.insert(0, self.filePath)
 
-	def continueTo(self):
+	def checkAndGetPath(self):
 		"""A set of instructions are ran when ok button is clicked. 
 	
 		"""
@@ -59,9 +62,12 @@ class browse(Frame):
 		# When entry is empty						
 		if self.filePath == "":
 			self.emptyFileNameWarning()
+			return False
 		# When file path is invaild meaning not a legit file
-		elif not isfile(self.filePath):
+		elif not exists(self.filePath):
 			self.incorrectFileNameWarning()
+			return False
+		return True
 
 	def closeWindow(self):
 		"""Close the toplevel window."""
@@ -101,17 +107,80 @@ class browseSheet(browse):
 	def __init__(self, root):
 		browse.__init__(self, root)
 		self.nextFrame = None
+		# b = browseFolder(root)
+		# c = saveXML(root)
 
 	def initGUI(self):
 		super().initGUI()
-		button = [{'text': 'Next', 'width': 7, 'function': self.continueTo}, {'text': 'Cancel', 'width': 10, 'function': self.closeWindow}]
+		button = [{'text': 'Cancel', 'width': 10, 'function': self.closeWindow}, {'text': 'Next', 'width': 7, 'function': self.toBrowseFolder}]
 		self.makeButtons(self.getFilePath, button)
 
-	def continueTo(self):
-		super().continueTo()
+	def getFilePath(self):
+		fileType1 = ("Comma Delimited CSV", "*.CSV")
+		fileType2 = ("Excel Workbook", "*.xlsx")
+		fileType3 = ("Excel Macro-Enabled Workbook", "*.xlsm")
+		fileType4 = ("All files", "*.*")
+		
+		self.filePath = askopenfilename(filetypes = (fileType1, fileType2, fileType3, fileType4), parent = self.root)
+
+		# Once self.filePath gets a filepath, delete what's in the entry and put self.filePath into the entry
+		self.filePathEntry.delete(0, 'end')
+		self.filePathEntry.insert(0, self.filePath)
+
+	def toBrowseFolder(self):
+		if self.checkAndGetPath():
+			self.lower()
+			b = browseFolder(self.root)
+			b.lift()
+
+class browseFolder(browse):
+
+	def __init__(self, root):
+		browse.__init__(self, root)
+		self.nextFrame = None
+
+	def initGUI(self):
+		super().initGUI()
+		button = [{'text': 'Cancel', 'width': 10, 'function': self.closeWindow}, {'text': 'Next', 'width': 7, 'function': self.toSaveXML}, {'text': 'Back', 'width': 7, 'function': self.back}]
+		self.makeButtons(self.getFilePath, button)
 
 	def getFilePath(self):
-		print("haha")
+		
+		self.filePath = askdirectory(parent = self.root)
+		print(self.filePath)
+
+		# Once self.filePath gets a filepath, delete what's in the entry and put self.filePath into the entry
+		self.filePathEntry.delete(0, 'end')
+		self.filePathEntry.insert(0, self.filePath)
+
+	def toSaveXML(self):
+		self.checkAndGetPath()
+
+class saveXML(browse):
+
+	def __init__(self, root):
+		browse.__init__(self, root)
+		self.nextFrame = None
+
+	def initGUI(self):
+		super().initGUI()
+		button = [{'text': 'Cancel', 'width': 10, 'function': self.closeWindow}, {'text': 'Save', 'width': 7, 'function': self.save}, {'text': 'Back', 'width': 7, 'function': self.back}]
+		self.makeButtons(self.getFilePath, button)
+
+	def getFilePath(self):
+		fileType1 = ("XML Data", "*.xml")
+		fileType2 = ("Text", "*.txt")
+		fileType3 = ("All files", "*.*")
+
+		self.filePath = asksaveasfilename(defaultextension= '.xml', filetypes = (fileType1, fileType2, fileType3), parent = self.root)
+		print(self.filePath)
+
+		# Once self.filePath gets a filepath, delete what's in the entry and put self.filePath into the entry
+		self.filePathEntry.delete(0, 'end')
+		self.filePathEntry.insert(0, self.filePath)
+
+	def save(self):
+		print(self.filePath)
 
 
 
@@ -124,6 +193,9 @@ class browseSheet(browse):
 window = Tk()
 window.title("CAKE")
 # Create a first object
-a = browseSheet(window)
+# a = browseSheet(window)
+# a.lift
+# b = browseFolder(window)
+c = saveXML(window)
 # Launch
 window.mainloop()
