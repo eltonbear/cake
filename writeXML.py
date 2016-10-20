c610cb702f291d67bdc94e6aae81c19e90d5b41f
SHON <elton.shon@keysight.com>
2016-10-20 03:57:23 -0700

    added three functions that calculate axes rotational angle, points coordinates translation, and part angle relative to new axes. added some data structures to include tip number and layer name and a dict of ref des to parts. and create multiple roots first according to its tip number. Also added comments. Also changed get camera Number function to include getting tip number. still need to finish combining roots in the end and save them into 3 seperate files. also need to change save xml file path to get a save folder path. And also need to incorprate the calculation funcs into the loop

--- writeXML.py
+++ writeXML.py
@@ -4,24 +4,40 @@ import math
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ElementTree, Element, SubElement
 
-def writeXml(version, excelData,dieFolderPath, XMLpath):
-	partIndex = 2
-	root = Element('Rec_SubstrateRecord')
-	versionElement = SubElement(root, 'Version')
-	versionElement.text = version
+def writeXml(excelData,dieFolderPath, XMLpath):
+	# Get the product name
 	productName = excelData['productName']
+	# Get a list of dictionaries of parts
+	parts = excelData['data']
+	# Create a root of the xml file
+	root = Element('Rec_SubstrateRecord')
+	# Create a version element and its content
+	versionElement = SubElement(root, 'Version')
+	versionElement.text = '7.6.22'
+	# Create an alignment name element with a content of the product name
 	alignmentNameElement = SubElement(root, 'AlignmentName')
 	alignmentNameElement.text = productName
-	parts = excelData['data']
+	# Get a list of die file names in the die folder
 	dieFileList = os.listdir(dieFolderPath)
-	dieToCameraNum = {}
-	missingDieFile = []
-	errorInFile = []
-	error = False
+
+	refDesToPartDict = {}              	# {'Ref Des': partDictionary}
+	layersToTipsToPartElementsDict = {} # {'layer':{'tip number': fake root}}
+	dieNameToTipAndCameraNumDict= {}	# {'dieName': {'camera number': camNum, 'tip number': tipNum}}
+	missingDieFile = []                 # A list of missing die file names if there any
+	errorInFile = []					# A list of error messages when the process is run if there is any
+	error = False						# Starts with no errors
+	# Iterate through all parts dictionaries in the list
 	for part in parts:
-		if part['Ref Des']:
+		# Get Ref Des value, and If 'Ref Des' value exists in the dictionary, do the following
+		refDes = part['Ref Des']
+		if refDes:
+			# Store parts in dictionary
+			refDesToPartDict[refDes] = part
+			# A die name is part number plus an underscore and the last four digit of the product name
 			dieName = part['Part Number'].replace('-', '') + '_' + productName[-4:]
-			if dieName not in dieToCameraNum:
+			# Grab the corresponding tip number and camera number to die name from the die xml file if not already looked up
+			if dieName not in dieNameToTipAndCameraNumDict:
+				# There are two possible file extension for a xml file (.txt and .xml). If both of these file paths not exist in the folder, gives an error.
 				fileName = ''
 				if dieName + '.XML' in dieFileList:
 					fileName = dieName + '.XML'
@@ -30,22 +46,55 @@ def writeXml(version, excelData,dieFolderPath, XMLpath):
 				else:
 					missingDieFile.append(dieName)
 					error = True
-					###### are there supposed to be all die files i need?
+					######################################################################## are there supposed to be all die files i need? 
+				# If file path is found, access the file and get the tip number and camera number. Gives errors if there are exceptions parsing the xml file or finding the desire elements
 				if fileName:
 					xmlPath = dieFolderPath + '/' + fileName
-					camNum = getCameraNum(xmlPath)
-					if camNum.isdigit(): # if camNum is str, then there is error in getCameraNum
-						dieToCameraNum[dieName] = camNum					
+					TipAndCamNum = getTipAndCameraNum(xmlPath)
+					if 'error' in TipAndCamNum: # if there is 'error' key in TipAndCamNum, then there is error in getCameraNum
+						errorInFile.append(TipAndCamNum['error'])
+						error = True					
 					else:
-						errorInFile.append(camNum)
-						error = True
+						dieNameToTipAndCameraNumDict[dieName] = TipAndCamNum
+			# If there isn't any errors throughout the previous processes, write a part element and append it to root of the xml tree.
 			if not error:
-				partElement = writePart(part, dieName, dieToCameraNum[dieName])
-				root.insert(partIndex, partElement)
-			##### MAYBE PUT LOCAL ALIGNMENT HERE
+				# Get layer name from dictionary
+				layer = part['Layer'] ######################################################### assume the title is called 'Layer'
+				# Get tip number from dictionary
+				tipNumber = TipAndCamNum['TipNbr']
+				# Change the layer name to 'localAlignment' if it's not either 'PCB' or 'Miboard'
+				if layer != 'PCB' or layer != 'Miboard':  ##################################### what are the names for layer
+					layer = 'localAlignment'
+				# Write a part element with given imformation
+				partElement = writePart(part, dieName, TipAndCamNum['CameraNbr'])
+
+				# If the layer ket is already in the dictionary				
+				if layer in layersToTipsToPartElementsDict:
+					# Get the dictionary of tip numbers to part elements for that one layer
+					partsOnOneLayer = layersToTipsToPartElementsDict[layer]
+					# If tip number key is in the dictionary
+					if tipNumber in partsOnOneLayer:
+						# Append the part to the root
+						partsOnOneLayer[tipNumber].append(partElement)
+					else:
+						# Create a sudo root for the xml file
+						fakeRoot = Element('Rec_SubstrateRecord')
+						# Append the part to the root
+						fakeRoot.append(partElement)
+						# Asign the tip number key to the root in the dictionary
+						partsOnOneLayer[tipNumber] = fakeRoot
+				else:
+					# Create a sudo root for the xml file
+					fakeRoot = Element('Rec_SubstrateRecord')
+					# Append the part to the root
+					fakeRoot.append(partElement)
+					# Asign the layer key to the tip number key to the root in the dictionary
+					layersToTipsToPartElementsDict[layer] = {tipNumber: fakeRoot}
+	# If there are any errors, return the error messaegs
 	if error:
 		return {'missingDie': missingDieFile, 'error': errorInFile}
 	else:
+		### combine trees useing extend. and save them into 3 different files
 		tree = ElementTree(root)
 		tree.write(XMLpath)
 		return {}
@@ -81,30 +130,62 @@ def writePart(partInfo, dieName, cameraNbr):
 
 	return partElement
 
-def getCameraNum(filePath):
+def getTipAndCameraNum(filePath):
 	try:
 		# Parse xml content
 		tree = ET.parse(filePath)                                    
 	except ET.ParseError as error: 
-		return error.args[0] + ' in file: ' + filePath
+		return {'error': error.args[0] + ' in file: ' + filePath}
 	# Get root of the xml data tree and all reference and wire elements if xml is parsed successfully
 	recDie = tree.getroot() 
 	camerNumElement = recDie.find('CameraNbr')
+	tipNumElement = recDie.find('TipNbr')
 	if camerNumElement != None:
-		return camerNumElement.text
+		if tipNumElement != None:
+			return {'CameraNbr': camerNumElement.text, 'TipNbr': tipNumElement.text}
+		else:
+			return {'error': 'No tip number in file: ' + filePath}
 	else:
-		return 'No camera number in file: ' + filePath
+		return {'error':'No camera number in file: ' + filePath}
 
-def axesRotationAngle(x1,x2,y1,y2):
+
+def calcAxesRotaionalAngle(x1,y1,x2,y2):
 	deltaX = x2-x1
 	deltaY = y2-y1
 
-	angle = math.atan2(deltaY, deltaX)
-	return angle
+	return math.atan2(deltaY, deltaX)
 
-# if __name__ == '__main__':
-# 	angle = math.radians(270)-axesRotationAngle(5, 8, 5, 1)
-# 	print(angle)
+def calcPartAngle(ogAngle, axesRotationalAngle):
+	
+	finalAngle = (ogAngle - axesRotationalAngle) % (2*math.pi)
+	if finalAngle > math.pi:
+		finalAngle = finalAngle - (2*math.pi)
+	print(math.degrees(axesRotationalAngle), math.degrees(finalAngle))	
+	return round(finalAngle, 8)
+
+def pointTranslation(x1, y1, xo, yo, axesRotationalAngle):
+	radius = math.sqrt((xo-x1)**2+(yo-y1)**2)
+	print(radius, ' r')
+	# The angle to the point is equal to the angle between new origin to the point and new x axis after axes are shift but not rotated - axesRotationalAngle 
+	pointAngle = calcAxesRotaionalAngle(xo, yo, x1, y1) - axesRotationalAngle
+	print('calcAxesRotaionalAngle', math.degrees(calcAxesRotaionalAngle(xo, yo, x1, y1)))
+	print('axesRotationalAngle', math.degrees(axesRotationalAngle))
+	print('pointAngle', math.degrees(pointAngle))
+	newX = math.cos(pointAngle) * radius
+	newY = math.sin(pointAngle) * radius
+	print(round(newX, 8), round(newY, 8))
+	return round(newX, 8), -round(newY, 8)
+
+if __name__ == '__main__':
+
+	# print(calcPartAngle(math.pi/2, -110/180*math.pi))
+	# a = calcAxesRotaionalAngle(5, 5 , 0, 10)
+	# print(math.degrees(a))
+	# b ,c  = pointTranslation(2, -2, 5, 5, a)
+	# a = getTipAndCameraNum(r'C:\Users\eltoshon\Desktop\Die\1GC14038_7740.xml')
+	# print(type(a['TipNbr']))
+	# print(a['TipNbr'])
+
 
 # how string will be in layer 3 (part name? ) also is this hard for the designer? 
 # How is the angle presented? there are negative and positive values
