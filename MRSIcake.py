84f430d2249b90a1ee84411a29ff60df83c49312
SHON <elton.shon@keysight.com>
2016-10-21 04:05:52 -0700

    cleaned print statements. changed parameters in writeXML.writeXml. make startfile into for loop to open each file.

--- MRSIcake.py
+++ MRSIcake.py
@@ -8,28 +8,23 @@ def runApp():
 	app.mainloop()
 	paths = app.getPaths()
 	openXML = app.ifOpenXML()
-	# print(paths, app.toContinue())
 	if app.toContinue():
 		partsDataFromExcel = excel.readSheet(paths['browseSheet'])
-		print(55)
 		if 'error' in partsDataFromExcel:
 			app.errorMessageWindow(partsDataFromExcel['error'])
-			print(44)
 		else:
-			error = writeXML.writeXml('version??', partsDataFromExcel, paths['browseFolder'], paths['saveXML'])
-			print(33)
-			if error:
-				if error['missingDie'] and error['error'] :
-					message =  '\n'.join(['Missing Die Files: '] + error['missingDie'] + ['\n', 'Error'] + error['error'])
-				elif error['missingDie']:
-					message =  '\n'.join(['Missing Die Files: '] + error['missingDie'])
+			ifError = writeXML.writeXml(partsDataFromExcel, paths['browseFolder'], paths['saveXML'])
+			if ifError:
+				if ifError['missingDie'] and ifError['error'] :
+					message = '\n'.join(['Missing Die Files: '] + ifError['missingDie'] + ['\n', 'Error'] + ifError['error'])
+				elif ifError['missingDie']:
+					message = '\n'.join(['Missing Die Files: '] + ifError['missingDie'])
 				else:
-					message =  '\n'.join(['Error'] + error['error'])
+					message = '\n'.join(['Error'] + ifError['error'])
 				app.errorMessageWindow(message)
-				print(22)
 			if openXML:
-				print(1111)
-				os.startfile(paths['saveXML'])
+				for xmlPath in ifError['path']:
+					os.startfile(xmlPath)
 
 if __name__ == "__main__":
 	runApp()
