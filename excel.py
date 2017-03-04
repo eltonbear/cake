6f31f9740a628b577786d7f9710d1a18e1278308
eltonbear <eltonbear@berkeley.edu>
2017-03-04 06:30:11 -0800

    got rid of sheet name checks, only check the number of sheets. got rid of extension checks. got rid of comments

--- excel.py
+++ excel.py
@@ -3,18 +3,14 @@ import os
 import re
 
 def readSheet(filePath):
-	fileBaseName = os.path.basename(filePath)
-	fileName, fileExtension = os.path.splitext(fileBaseName) ############################################# FIRST SHEET NAME????????????/
-	if fileExtension == '.csv':
-		return {'error': '.csv file type does not support multiple sheets. Please save the file in .xlsx.'}
 	try:
-		# Get workbook that has two sheets
+		# Get workbook
 		book = pe.get_book(file_name=filePath) 
-		
-		# if 'Alignment' not in book.sheet_names():
-		# 	return {'error': 'File: ' + filePath + ' missing "Alignment" worksheet.'}
-		if fileName not in book.sheet_names():
-			return {'error': 'File: ' + filePath + ' missing "' + fileName + '" worksheet.'}
+		# Get sheet name in list
+		sheetName = book.sheet_names()
+		# error if there are more than 1 sheet in this book
+		if len(sheetName) > 1:
+			return {'error': 'There are more than one sheet.'}
 
 	except NotImplementedError as error:
 		return {'error': 'No source found for ' + filePath}
@@ -22,25 +18,19 @@ def readSheet(filePath):
 		return {'error': error.args[1] + ': ' + filePath}
 
 	partsTitleRow = 1
-	partSheet = book[fileName]
-	# alignmentSheet = book['Alignment']
+	# Get sheet
+	partSheet = book[sheetName[0]]
 	partSheet.name_columns_by_row(partsTitleRow)
-	# alignmentSheet.name_columns_by_row(0)
 
 	partsTitleRow = -1
 	productInfoC = 0
 	productInfoR = 0
 	emptyRow = 1
 	requiredPartTitles = ['Ref Des', 'Part Number', 'X-location', 'Y-location', 'Rotation', 'Layer']
-	# requriedAlignTitles = ['Layer',	'x1', 'y1', 'x2', 'y2']
 	partSheet.colnames = list(map(str.strip, partSheet.colnames))
-	# alignmentSheet.colnames = list(map(str.strip, alignmentSheet.colnames))
 	partTitles = partSheet.colnames
-	# alignmentTitles = alignmentSheet.colnames
 	emptyRowList = partSheet.row[emptyRow]
 	missingPartColumns = []
-	# missingAlignColumns = []
-
 
 	# Check if required columns all exist for partSheet and alignment sheet
 	for requireTitle in requiredPartTitles:
@@ -48,11 +38,6 @@ def readSheet(filePath):
 			missingPartColumns.append(requireTitle)
 	if missingPartColumns:	
 		return{'error': 'File missing column of "' + ','.join(missingPartColumns) + '" in parts sheet!'}
-	# for requireTitle in requriedAlignTitles:
-	# 	if requireTitle not in alignmentTitles:
-	# 		missingAlignColumns.append(requireTitle)
-	# if missingAlignColumns:	
-	# 	return{'error': 'File missing column of "' + ','.join(missingAlignColumns) + '" in alignment sheet!'}
 
 	# Check if the empty row in parts sheet is a list of strings
 	if not all(isinstance(e, str) for e in emptyRowList):
@@ -67,9 +52,7 @@ def readSheet(filePath):
 		productName = productionInfo[0]
 		del partSheet.row[emptyRow]
 		del partSheet.row[productInfoR]
-		# partsDictionaries = partSheet.to_records()
 		partsAndFIDdictionaries = partSheet.to_records()
-		# alignmentDictsTemp = alignmentSheet.to_records()
 	except:
 		return {'error': 'File format incorrect: ' + filePath}
 
@@ -79,7 +62,7 @@ def readSheet(filePath):
 	for part in partsAndFIDdictionaries:
 		if part['Ref Des'][:3] == 'FID':
 			alignmentDictsTemp.append(part)
-		else:
+		elif part['Ref Des'] != '':
 			partsDictionaries.append(part)
 
 	# Separate FIDs by layers --> {'L1': [D1, D2], 'L2':[D1 D2], ....}, D1 = {'Ref Des': 'FID#', .............} 
@@ -104,11 +87,8 @@ def readSheet(filePath):
 			alignmentDictionaries[fidLayer] = {'p1': (fid2['X-location'], fid2['Y-location']), 'p2': (fid1['X-location'], fid1['Y-location'])}
 		else:
 			alignmentDictionaries[fidLayer] = {'p1': (fid1['X-location'], fid1['Y-location']), 'p2': (fid2['X-location'], fid2['Y-location'])}
-	
-	return {'productName': productName, 'data': partsDictionaries, 'fiducials': alignmentDictionaries}
 
-if __name__ == "__main__":
-	x = readSheet(r'C:\Users\eltonbear\Desktop\pico_top_test.xlsx')
+	return {'productName': productName, 'data': partsDictionaries, 'fiducials': alignmentDictionaries}
 
 
 
